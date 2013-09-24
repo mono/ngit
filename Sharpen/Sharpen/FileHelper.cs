mono/ngit

commit dd4f4c3580588bd1fa40c198ac33afc7fa41ae69

[Sharpen] Load UnixFileHelper correctly in ASP.NET
Load the file helper correctly in ASP.NET scenarios. Without specifying the assembly name, it would fail to load every time.

Sharpen/Sharpen/FileHelper.cs
@@ -15,7 +15,7 @@ namespace Sharpen
 			if (Environment.OSVersion.Platform.ToString ().StartsWith ("Win"))
 				Instance = new FileHelper ();
 			else {
-				var ufh = Type.GetType("Sharpen.Unix.UnixFileHelper");
+				var ufh = Type.GetType("Sharpen.Unix.UnixFileHelper, Sharpen.Unix");
 				if (ufh == null) {
 					var path = ((FilePath) typeof (FileHelper).Assembly.Location).GetParent();
 					var assembly = Assembly.LoadFile(Path.Combine(path, "Sharpen.Unix.dll"));

commit ff1e37309b0235ed6e7a63e2a8ee3fc91e2be00f

Fix a regression in merged assemblies introduced in commit dd4f4c3

Sharpen/Sharpen/FileHelper.cs
@@ -15,7 +15,10 @@ namespace Sharpen
 			if (Environment.OSVersion.Platform.ToString ().StartsWith ("Win"))
 				Instance = new FileHelper ();
 			else {
-				var ufh = Type.GetType("Sharpen.Unix.UnixFileHelper, Sharpen.Unix");
+				var ufh = Type.GetType("Sharpen.Unix.UnixFileHelper");
+				if (ufh == null) {
+					ufh = Type.GetType("Sharpen.Unix.UnixFileHelper, Sharpen.Unix");
+				}
 				if (ufh == null) {
 					var path = ((FilePath) typeof (FileHelper).Assembly.Location).GetParent();
 					var assembly = Assembly.LoadFile(Path.Combine(path, "Sharpen.Unix.dll"));